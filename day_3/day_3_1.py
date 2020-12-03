2d82428b3e78d584b4c514899f2cffd5458d290e
Nicolai Eliassen Pedersen <nicolaiep@uia.no>
2020-12-03 23:09:46 +0100

    Refactored the name of each input file

--- day_3/day_3_1.py
+++ day_3/day_3_1.py
@@ -1,4 +1,4 @@
-with open("day_3/day_3.txt") as f:
+with open("day_3/day_3_input.txt") as f:
     terrain = tuple(line for line in f.read().splitlines())
 
 print(sum(row[3 * i % len(row)] == "#" for i, row in enumerate(terrain)))
