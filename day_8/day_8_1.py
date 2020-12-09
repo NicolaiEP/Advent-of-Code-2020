ee7efe98e565e34e38c797176045730d599ed0b5
Nicolai Eliassen Pedersen <nicolaiep@uia.no>
2020-12-09 14:55:52 +0100

    Simplified the first solution for day 8 a little

--- day_8/day_8_1.py
+++ day_8/day_8_1.py
@@ -1,19 +1,20 @@
 with open("day_8/day_8_input.txt") as f:
-    lines = list(enumerate(line.split() for line in f.read().splitlines()))
+    lines = [line.split() for line in f.read().splitlines()]
 
 visited = set()
 accumulator = 0
 index = 0
 while True:
-    i, instruction = lines[index]
-    if i in visited:
+    if index in visited:
         print(accumulator)
         break
-    elif instruction[0] == "acc":
-        accumulator += int(instruction[1])
-        index += 1
-    elif instruction[0] == "jmp":
-        index += int(instruction[1])
     else:
-        index += 1
-    visited.add(i)
+        visited.add(index)
+        operator, argument = lines[index]
+        if operator == "acc":
+            accumulator += int(argument)
+            index += 1
+        elif operator == "jmp":
+            index += int(argument)
+        else:
+            index += 1
