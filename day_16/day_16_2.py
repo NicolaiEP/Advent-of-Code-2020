8f8b0b3e4269055c845d2371c06dc4503a5befd4
Nicolai Eliassen Pedersen <nicolaiep@uia.no>
2021-01-26 12:04:56 +0100

    Cleaned the code a little and added comments

--- day_16/day_16_2.py
+++ day_16/day_16_2.py
@@ -1,28 +1,27 @@
 with open("day_16/day_16_input.txt") as f:
-    fields, my_ticket, other_tickets = f.read().split("\n\n")
+    field_numbers, my_ticket, other_tickets = f.read().split("\n\n")
 
-valid_numbers = set()
+# Creates number lists from the strings
+my_ticket = [int(n) for n in my_ticket.split("\n")[1].split(",")]
+other_tickets = [[int(n) for n in line.split(",")] for line in other_tickets.split("\n")[1:]]
+
+# Creates a set and dictionary for valid numbers
 field_ranges = {}
-for field in fields.split("\n"):
+for field in field_numbers.split("\n"):
     *field_name, first_range, _, second_range = field.split()
     first_range = range(*[int(n) + i for i, n in enumerate(first_range.split("-"))])
     second_range = range(*[int(n) + i for i, n in enumerate(second_range.split("-"))])
-    valid_numbers.update(first_range, second_range)
     field_ranges[" ".join(field_name).rstrip(":")] = set(first_range) | set(second_range)
 
+# Transposes the valid ticket list
+valid_numbers = set.union(*field_ranges.values())
+valid_tickets = [ticket for ticket in other_tickets if set(ticket) <= valid_numbers]
+field_numbers = [set(field) for field in zip(*valid_tickets)]
 
-valid_tickets = []
-for ticket in other_tickets.split("\n")[1:]:
-    ticket = [int(n) for n in ticket.split(",")]
-    if all(n in valid_numbers for n in ticket):
-        valid_tickets.append(ticket)
-
-fields = [set(field) for field in zip(*valid_tickets)]
-
+# Finds the indexes of each field
 field_positions = {}
-number_of_fields = len(field_ranges)
-while len(field_positions) < number_of_fields:
-    for i, field in enumerate(fields):
+while len(field_positions) < len(my_ticket):
+    for i, field in enumerate(field_numbers):
         potential_fields = []
         for field_name, values in field_ranges.items():
             if field.issubset(values):
@@ -32,7 +31,7 @@ while len(field_positions) < number_of_fields:
             field_positions[field_name] = i
             del field_ranges[field_name]
 
-my_ticket = [int(n) for n in my_ticket.split("\n")[1].split(",")]
+# Finds the product
 product = 1
 for field_name, i in field_positions.items():
     if field_name.startswith("departure"):
