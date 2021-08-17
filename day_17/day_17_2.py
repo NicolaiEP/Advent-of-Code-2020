991471968397941e6f4222e9e72b17b76f34dfbd
Nicolai Eliassen Pedersen <nicolaiep@uia.no>
2021-08-17 16:29:55 +0200

    Updated the names of a function and some variables

--- day_17/day_17_2.py
+++ day_17/day_17_2.py
@@ -13,13 +13,13 @@ n_dimensions = 4
 relative_positions = set(product((-1, 0, 1), repeat=n_dimensions)) - {(0,) * n_dimensions}
 
 
-def get_neighbourhood(pos):
+def get_neighbours(pos):
     """Return a set of the n-dimensional neighbourhood positions of a given position pos."""
     neighbours = {tuple(sum(x) for x in zip(pos, offset)) for offset in relative_positions}
     return neighbours
 
 
-# Saves the positions and their neighbour positions of the active cubes in a dict
+# Saves the positions and their neighbour positions of the active cells in a dict
 positions = [(x, y, z, w) for x in range(WIDTH) for y in range(HEIGHT)
              for z in range(DEPTH) for w in range(W_DEPTH)]
 
@@ -33,7 +33,7 @@ for pos, state in zip(positions, chain(*lines)):
     if not state == "#":
         continue
 
-    neighbours = get_neighbourhood(pos)
+    neighbours = get_neighbours(pos)
     active_cells[pos] = neighbours
     for neigbour in neighbours:
         if neigbour not in interacted_cells:
@@ -58,7 +58,7 @@ for _ in range(ITERATION_COUNT):
 
     # Updates the pocket dimension with new cells
     for cell in new_cells:
-        neighbours = get_neighbourhood(cell)
+        neighbours = get_neighbours(cell)
         active_cells[cell] = neighbours
         for neigbour in neighbours:
             if neigbour not in interacted_cells:
@@ -84,39 +84,39 @@ print("FINISH\n")
 
 # APPROACH 2 (OLD):
 
-# Saves the positions of active cubes
-active_cubes = {pos for pos, state in zip(positions, chain(*lines)) if state == "#"}
+# Saves the positions of active cells
+active_cells = {pos for pos, state in zip(positions, chain(*lines)) if state == "#"}
 
 
 for _ in range(6):
     start_time = time.time()
-    # checked_cubes = set()
-    new_cubes = set()
-    dying_cubes = set()
-    # for cube in active_cubes:
-    #     if cube in checked_cubes:
+    # checked_cells = set()
+    new_cells = set()
+    dying_cells = set()
+    # for cell in active_cells:
+    #     if cell in checked_cells:
     #         continue
-    #     neighbours = get_neighbourhood(cube)
-    #     count = len(neighbours & active_cubes)
+    #     neighbours = get_neighbourhood(cell)
+    #     count = len(neighbours & active_cells)
 
-    # Finds the cubes to consider this cycle
-    affected_cubes = set(active_cubes).union(*(get_neighbourhood(cube) for cube in active_cubes))
+    # Finds the cells to consider this cycle
+    affected_cells = set(active_cells).union(*(get_neighbours(cell) for cell in active_cells))
 
     # Updates the sets for activation and deactivation
-    for pos in affected_cubes:
+    for pos in affected_cells:
         # Counts active neighbours of pos
-        count = len(get_neighbourhood(pos) & active_cubes)
+        count = len(get_neighbours(pos) & active_cells)
 
         # Decides if pos is going to activate, deactivate or be ignored
-        active = pos in active_cubes
+        active = pos in active_cells
         if active and not 2 <= count <= 3:
-            dying_cubes.add(pos)
+            dying_cells.add(pos)
         elif not active and count == 3:
-            new_cubes.add(pos)
+            new_cells.add(pos)
 
-    active_cubes = new_cubes | active_cubes - dying_cubes
+    active_cells = new_cells | active_cells - dying_cells
 
-    print(len(active_cubes))
+    print(len(active_cells))
     print("Iteration time:", time.time() - start_time)
 
 print("FINISH\n")
@@ -129,19 +129,19 @@ for _ in range(6):
     start_time = time.time()
     new_state = state_dict.copy()
 
-    # Finds the positions of the cubes to consider this cycle
-    affected_cubes = set.union(*(get_neighbourhood(pos) for pos in state_dict.keys()))
+    # Finds the positions of the cells to consider this cycle
+    affected_cells = set.union(*(get_neighbours(pos) for pos in state_dict.keys()))
 
-    # Updates the states of the affected cubes
-    for pos in affected_cubes:
+    # Updates the states of the affected cells
+    for pos in affected_cells:
         # Counts active neighbours
-        count = sum(state_dict.get(p, ".") == "#" for p in get_neighbourhood(pos))
-        cube_state = state_dict.get(pos, ".")
+        count = sum(state_dict.get(p, ".") == "#" for p in get_neighbours(pos))
+        cell_state = state_dict.get(pos, ".")
 
-        # Updates the cube state according to the rules
-        if cube_state == "#" and 2 <= count <= 3:
+        # Updates the cell state according to the rules
+        if cell_state == "#" and 2 <= count <= 3:
             continue
-        elif cube_state == "." and count == 3:
+        elif cell_state == "." and count == 3:
             new_state[pos] = "#"
         else:
             new_state[pos] = "."
