c418ff5f0822e6fb4a27ceea62c9d152e2c7d10b
Nicolai Eliassen Pedersen <nicolaiep@uia.no>
2021-08-17 16:49:08 +0200

    Updated the first solution for day 18 with a faster approach

--- day_17/day_17_1.py
+++ day_17/day_17_1.py
@@ -1,45 +1,81 @@
 from itertools import product, chain
+import time
 
 with open("day_17/day_17_input.txt") as f:
     lines = [list(line) for line in f.read().splitlines()]
 
+ITERATION_COUNT = 6
 HEIGHT = len(lines)
 WIDTH = len(lines[0])
 DEPTH = 1
+n_dimensions = 3
+relative_positions = set(product((-1, 0, 1), repeat=n_dimensions)) - {(0,) * n_dimensions}
 
 
-def get_neighbourhood(pos):
+def get_neighbours(pos):
     """Return a set of the n-dimensional neighbourhood positions of a given position pos."""
-    n_dimensions = len(pos)
-    relative_positions = set(product((-1, 0, 1), repeat=n_dimensions)) - {(0,) * n_dimensions}
     neighbours = {tuple(sum(x) for x in zip(pos, offset)) for offset in relative_positions}
     return neighbours
 
 
-# Saves the positions and the initial states of the cubes in a dict
-positions = [(x, y, z) for x in range(WIDTH) for y in range(HEIGHT) for z in range(DEPTH)]
-state_dict = {pos: state for pos, state in zip(positions, chain(*lines))}
+# Saves the positions and their neighbour positions of the active cells in a dict
+positions = [(x, y, z) for x in range(WIDTH) for y in range(HEIGHT)
+             for z in range(DEPTH)]
 
-for _ in range(6):
-    new_state = state_dict.copy()
 
-    # Finds the positions of the cubes to consider this cycle
-    affected_cubes = set.union(*(get_neighbourhood(pos) for pos in state_dict.keys()))
+# APPROACH 1 (NEW):
 
-    # Updates the states of the affected cubes
-    for pos in affected_cubes:
-        # Counts active neighbours
-        count = sum(state_dict.get(p, ".") == "#" for p in get_neighbourhood(pos))
-        cube_state = state_dict.get(pos, ".")
+# Adds cells with a count of nearby active cells. Active cells also gets an own dict with neighbour cells
+interacted_cells = {}
+active_cells = {}
+for pos, state in zip(positions, chain(*lines)):
+    if not state == "#":
+        continue
 
-        # Updates the cube state according to the rules
-        if cube_state == "#" and 2 <= count <= 3:
-            continue
-        elif cube_state == "." and count == 3:
-            new_state[pos] = "#"
+    neighbours = get_neighbours(pos)
+    active_cells[pos] = neighbours
+    for neigbour in neighbours:
+        if neigbour not in interacted_cells:
+            interacted_cells[neigbour] = 1
         else:
-            new_state[pos] = "."
+            interacted_cells[neigbour] += 1
 
-    state_dict = new_state
+for _ in range(ITERATION_COUNT):
+    start_time = time.time()
 
-print(sum(value == "#" for value in state_dict.values()))
+    # Adds dying cells to a set
+    dying_cells = set()
+    for cell in active_cells:
+        if cell not in interacted_cells or not 2 <= interacted_cells[cell] <= 3:
+            dying_cells.add(cell)
+
+    # Adds new cells to a set
+    new_cells = set()
+    for cell, value in interacted_cells.items():
+        if cell not in active_cells and value == 3:
+            new_cells.add(cell)
+
+    # Updates the pocket dimension with new cells
+    for cell in new_cells:
+        neighbours = get_neighbours(cell)
+        active_cells[cell] = neighbours
+        for neigbour in neighbours:
+            if neigbour not in interacted_cells:
+                interacted_cells[neigbour] = 1
+            else:
+                interacted_cells[neigbour] += 1
+
+    # Updates the pocket dimension with dead cells
+    for cell in dying_cells:
+        neighbours = active_cells[cell]
+        del active_cells[cell]
+        for neigbour in neighbours:
+            if interacted_cells[neigbour] == 1:
+                del interacted_cells[neigbour]
+            else:
+                interacted_cells[neigbour] -= 1
+
+    print(len(active_cells))
+    print("Iteration time:", time.time() - start_time)
+
+print("FINISH\n")
