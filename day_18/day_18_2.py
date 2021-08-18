1222af867378663d9a4e556b2920b87675ef3453
Nicolai Eliassen Pedersen <nicolaiep@uia.no>
2021-08-19 00:24:57 +0200

    Added the second solution for day 18 (not day 17)

---
+++ day_18/day_18_2.py
@@ -0,0 +1,49 @@
+import re
+
+with open("day_18/day_18_input.txt") as f:
+    lines = [re.findall(r"(\d+|[^ 0-9])", line) for line in f.read().splitlines()]
+
+
+def shunting_yard(expr):
+    """Convert an infix math expression to postfix."""
+    output = []
+    operator_stack = []
+    for token in expr:
+        if token.isnumeric():
+            output.append(token)
+        elif token in ("+", "*"):
+            while operator_stack and operator_stack[-1] == "+" and token == "*":
+                output.append(operator_stack.pop())
+            operator_stack.append(token)
+        elif token == "(":
+            operator_stack.append(token)
+        elif token == ")":
+            while operator_stack[-1] != "(":
+                assert operator_stack, "Mismatched parenthesis."
+                output.append(operator_stack.pop())
+            assert operator_stack[-1] == "("
+            operator_stack.pop()
+
+    while operator_stack:
+        output.append(operator_stack.pop())
+
+    return output
+
+
+def calc_postfix(expr):
+    """Calculate a math expression with postfix notation."""
+    stack = []
+    for token in expr:
+        if token in ("+", "*"):
+            first = stack.pop()
+            second = stack.pop()
+            if token == "+":
+                stack.append(first.__add__(second))
+            elif token == "*":
+                stack.append(first.__mul__(second))
+        elif token:
+            stack.append(int(token))
+    return stack.pop()
+
+
+print(sum(calc_postfix(shunting_yard(line)) for line in lines))
